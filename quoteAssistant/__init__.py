350a005587fed6489c9161765a8fe54a153eddd4
meehanjohn <meehan6@illinois.edu>
2019-04-23 21:26:43 -0400

    Unsettling change to __main__

--- quoteAssistant/__init__.py
+++ quoteAssistant/__init__.py
@@ -2,6 +2,6 @@ from .carry_over import carry_over
 from .create_job_list import create_job_list
 from .get_stuff import materials, operations, subcontracts
 from .load_spreadsheet import load_spreadsheet
-from .main import main
+from .__main__ import main
 from .read_file import read_file
 from .make_table import make_table
